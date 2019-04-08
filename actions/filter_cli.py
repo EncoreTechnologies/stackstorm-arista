29948984ed41c131873d35508754fb31cd3e723b
Nick Maludy <nmaludy@gmail.com>
2019-04-08 15:50:59 -0400

    Working on cli workflow and formatting

--- actions/filter_cli.py
+++ actions/filter_cli.py
@@ -4,12 +4,13 @@ import json
 
 class FilterCli(Action):
 
-    def run(self, cli_result, command, raw):
+    def run(self, cli_result, command, hosts, raw):
         results = []
-        for r in cli_result:
+        for idx, r in enumerate(cli_result):
             if command in r:
                 output = r[command].replace('\\n', '\n')
                 if not raw:
                     output = json.loads(output)
-                results.append(output)
+                results.append({'host': hosts[idx],
+                                'result': output})
         return results
