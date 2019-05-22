9f8c5da41da832523d0cc1c03bb4cb4b6f3e1996
Nick Maludy <nmaludy@gmail.com>
2019-05-22 08:21:17 -0400

    Working on ansible actions

---
+++ actions/filter_ansible.py
@@ -0,0 +1,29 @@
+from st2common.runners.base_action import Action
+import six
+
+
+class FilterAnsible(Action):
+
+    def run(self, result, tasks):
+        extracted = []
+
+        for play in result['plays']:
+            for task in play['tasks']['tasks']:
+                task_name = task['name']
+                task_extract_details = next(t for t in tasks if t['name'] == task_name)
+                if not task_extract_details:
+                    continue
+
+                task_extract = {}
+                for host, vars_dict in six.iteritems(task['hosts']):
+                    if task_extract_details['vars']:
+                        task_extract[host] = {}
+                        for var in task_extract_details['vars']:
+                            task_extract[host][var] = vars_dict[var]
+                    else:
+                        task_extract[host] = vars_dict
+
+                extracted.push_back(task_extract)
+
+        success = True if extracted else False
+        return (success, extracted)
